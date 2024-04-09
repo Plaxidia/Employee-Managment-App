cc3c223aefe49d5bf7b877158d603df0abbf5a3e
Plaxidia Hove <90321105+Plaxidia@users.noreply.github.com>
2024-04-10 00:05:26 +0200

    completed styling the home page

--- front_end/src/pages/home.js
+++ front_end/src/pages/home.js
@@ -2,13 +2,18 @@ import { Box, Button } from "@mui/material";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
-import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp";
+//import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp";
 import axios from "axios";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+//import TextField from "@mui/material/TextField";
 //import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
 import React, { useEffect, useState } from "react";
 import {
   TableContainer,
@@ -16,7 +21,7 @@ import {
   TableHead,
   TableBody,
   TableRow,
-  TableFooter,
+  // TableFooter,
   TableCell,
   Paper,
 } from "@mui/material";
@@ -27,19 +32,19 @@ function Home() {
   const StyledTableContainer = styled(TableContainer)({
     minWidth: 450,
     maxWidth: 1050,
-    minHeight:400,
+    minHeight: 400,
     maxHeight: 550,
-    //marginLeft: 100,
+
     border: "1px solid black",
   });
   const CenteredContainer = styled("div")({
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
+    marginTop: 10,
+    //alignItems: "center",
     height: "100vh", // Adjust as needed to center vertically
   });
 
-  //const classes = useStyles();
   const [rows, setRows] = useState([]); // State to store fetched data
 
   useEffect(() => {
@@ -55,161 +60,174 @@ function Home() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Disable scrolling when the component mounts
+    document.body.style.overflow = "hidden";
+    // Re-enable scrolling when the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
-    <Box>
-      <Box sx={{ flexGrow: 1, backgroundColor: "lightblue" }}>
-        <AppBar
-          position="static"
-          sx={{ backgroundColor: "darktblue", color: "black" }}
-        >
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Employee Managmment App
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Box
-        sx={{
-          marginTop: 4,
-          display: "flex",
-          alignItems: "",
-        }}
-      >
-        <TextField id="demo-helper-text-aligned" label="Search" />
-        <Button> add employee</Button>
-      </Box>
-
-      <CenteredContainer>
-        <StyledTableContainer component={Paper}>
-          <Table
-            aria-label="table with sticky header"
-            stickyHeader
-            stickyFooter
-            stripe="odd"
-            hoverRow
+    <div>
+      <Box>
+        <Box sx={{ flexGrow: 1, 
+          //backgroundColor: "lightblue",
+           marginTop: 4 }}>
+          <AppBar
+            position="static"
+            sx={{ backgroundColor: "grey", color: "white" }}
           >
-            <TableHead
-              sx={{ backgroundColor: "lightblue", position: "relative" }}
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Employee Managmment App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        {/* <Box
+          sx={{
+            marginTop: 1,
+            marginBottom: 0,
+            display: "flex",
+            //alignItems: "",
+          }}
+        >
+          <TextField id="demo-helper-text-aligned" label="Search" />
+          <Button> add employee</Button>
+        </Box> */}
+        {/* Search and button */}
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            //display: "flex",
+            marginLeft: 23,
+            marginTop:2,
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
+            minWidth: 450,
+            maxWidth: 1050,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search an Employee"
+            inputProps={{ "aria-label": "search an" }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <Button  sx={{ 
+            color:"white",
+            p: "10px" ,
+            background: "grey",
+            "&:hover": {
+              color: "white",
+              background: "lightgrey",
+            },}}>
+            Add New employee
+          </Button>
+        </Paper>
+
+        {/* table */}
+        <CenteredContainer>
+          <StyledTableContainer component={Paper}>
+            <Table
+              aria-label="table with sticky header"
+              stickyHeader
+              stickyFooter
+              stripe="odd"
+              hoverRow
             >
-              <TableRow
-                sx={{
-                  "& th": {
-                    color: "black",
-                    fontSize: 20,
-                    backgroundColor: "lightblue",
-                  },
-                }}
+              <TableHead
+                sx={{ position: "relative" }}
               >
-                <TableCell>First Name</TableCell>
-                <TableCell>Last Name</TableCell>
-                <TableCell>Gender</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Job Title</TableCell>
-                <TableCell>Department</TableCell>
-                <TableCell align="center">Actions</TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {rows.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell>{row.first_name}</TableCell>
-                  <TableCell>{row.last_name}</TableCell>
-                  <TableCell>{row.email}</TableCell>
-                  <TableCell>{row.gender}</TableCell>
-
-                  <TableCell>{row.job_title}</TableCell>
-                  <TableCell>{row.department}</TableCell>
-                  <TableCell>
-                    <td>
-                      <Box sx={{ display: "flex", width: "50%", gap: 1 }}>
-                        <VisibilityOutlinedIcon
-                          sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            borderRadius: "50%",
-                            alignItems: "center",
-                            cursor: "pointer",
-                            "&:hover": {
-                              color: "green",
-                              background: "#e6e2f0",
-                            },
-                          }}
-                        />
-                        <ModeEditOutlineOutlinedIcon
-                          sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            borderRadius: "50%",
-                            alignItems: "center",
-                            cursor: "pointer",
-                            "&:hover": {
-                              color: "Blue",
-                              background: "#e6e2f0",
-                            },
-                          }}
-                        />
-                        <DeleteOutlineOutlinedIcon
-                          sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            borderRadius: "50%",
-                            alignItems: "center",
-                            cursor: "pointer",
-                            "&:hover": {
-                              color: "red",
-                              background: "#e6e2f0",
-                            },
-                          }}
-                        />
-                      </Box>
-                    </td>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter
-              sx={{
-                maxWidth: 100,
-                position: "sticky",
-                bottom: 0,
-                backgroundColor: "lightblue",
-              }}
-            >
-              <TableRow>
-                <TableCell
-                  colSpan={10}
-                  sx={{ fontSize: "small", textAlign: "right" }}
+                <TableRow
+                  sx={{
+                    "& th": {
+                      color: "white",
+                      fontSize: 20,
+                      backgroundColor: "grey",
+                    },
+                  }}
                 >
-                  <Button
-                    variant="soft"
-                    sx={{
-                      top: 0,
-                      right: 0,
-                      marginRight: 4,
-                      width: "8%",
-                      color: "black",
-                      backgroundColor: "transparent",
-                      "&:hover": {
-                        // Override hover styles with empty values
-                        color: "black",
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Gender</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Job Title</TableCell>
+                  <TableCell>Department</TableCell>
+                  <TableCell align="center">Actions</TableCell>
+                </TableRow>
+              </TableHead>
 
-                        border: '2px solid "black" ',
-                        background: "grey",
-                      },
-                    }}
-                  >
-                    <ArrowBackIosNewSharpIcon />
-                    Back
-                  </Button>
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </StyledTableContainer>
-      </CenteredContainer>
-    </Box>
+              <TableBody>
+                {rows.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{row.first_name}</TableCell>
+                    <TableCell>{row.last_name}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                    <TableCell>{row.gender}</TableCell>
+
+                    <TableCell>{row.job_title}</TableCell>
+                    <TableCell>{row.department}</TableCell>
+                    <TableCell>
+                      <td>
+                        <Box sx={{ display: "flex", width: "50%", gap: 1 }}>
+                          <VisibilityOutlinedIcon
+                            sx={{
+                              display: "flex",
+                              justifyContent: "center",
+                              borderRadius: "50%",
+                              alignItems: "center",
+                              cursor: "pointer",
+                              "&:hover": {
+                                color: "green",
+                                background: "#e6e2f0",
+                              },
+                            }}
+                          />
+                          <ModeEditOutlineOutlinedIcon
+                            sx={{
+                              display: "flex",
+                              justifyContent: "center",
+                              borderRadius: "50%",
+                              alignItems: "center",
+                              cursor: "pointer",
+                              "&:hover": {
+                                color: "Blue",
+                                background: "#e6e2f0",
+                              },
+                            }}
+                          />
+                          <DeleteOutlineOutlinedIcon
+                            sx={{
+                              display: "flex",
+                              justifyContent: "center",
+                              borderRadius: "50%",
+                              alignItems: "center",
+                              cursor: "pointer",
+                              "&:hover": {
+                                color: "red",
+                                background: "#e6e2f0",
+                              },
+                            }}
+                          />
+                        </Box>
+                      </td>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </StyledTableContainer>
+        </CenteredContainer>
+      </Box>
+    </div>
   );
 }
 
@@ -272,4 +290,42 @@ export default Home;
                         }}
                       >
                         Delete
-                      </Button> */
+                      </Button> 
+                      
+                      <TableFooter
+              sx={{
+                maxWidth: 100,
+                position: "sticky",
+                bottom: 0,
+                //backgroundColor: "lightblue",
+              }}
+            >
+              <TableRow>
+                <TableCell
+                  colSpan={10}
+                  sx={{ fontSize: "small", textAlign: "right" }}
+                >
+                  <Button
+                    variant="soft"
+                    sx={{
+                      top: 0,
+                      right: 0,
+                      marginRight: 4,
+                      width: "8%",
+                      color: "black",
+                      backgroundColor: "transparent",
+                      "&:hover": {
+                        // Override hover styles with empty values
+                        color: "black",
+
+                        border: '2px solid "black" ',
+                        background: "grey",
+                      },
+                    }}
+                  >
+                    <ArrowBackIosNewSharpIcon />
+                    Back
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableFooter>*/
