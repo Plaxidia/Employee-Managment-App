4b664c4ae7032a181dcb7680d37a17abb4e2ae36
Plaxidia Hove <90321105+Plaxidia@users.noreply.github.com>
2024-05-13 23:35:36 +0200

    completed building the add employee page

--- front_end/src/pages/AddEmployee.js
+++ front_end/src/pages/AddEmployee.js
@@ -1,15 +1,15 @@
-import React, {useEffect } from "react";
-//import Modal from 'react-bootstrap/Modal';
-import { useNavigate } from "react-router-dom";
-import { Box, Button,  } from "@mui/material";
-import Card from '@mui/material/Card';
 
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button } from "@mui/material";
+import Card from "@mui/material/Card";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import TextField from '@mui/material/TextField';
-export default function AddEmployee (props ){
+import TextField from "@mui/material/TextField";
+import axios from "axios";
 
+export default function AddEmployee(props) {
   useEffect(() => {
     // Disable scrolling when the component mounts
     document.body.style.overflow = "hidden";
@@ -17,431 +17,218 @@ export default function AddEmployee (props ){
     return () => {
       document.body.style.overflow = "auto";
     };
-   }, []);
+  }, []);
 
   const navigate = useNavigate();
 
   const handleCancel = () => {
-            // // Navigate back to the home page
-            navigate('/');
+    // Navigate back to the home page
+    navigate("/");
   };
- 
-    return(
-      <div>
 
-      <Box sx={{ 
-        flexGrow: 1, 
-         }}>
-       <AppBar
-         position="static"
-         sx={{ backgroundColor: "grey", color: "white" }}
-         >
-         <Toolbar>
-           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-             Employee Managmment App
-           </Typography>
-         </Toolbar>
-       </AppBar>
-     </Box>
-<Card 
-        sx={{ 
+  const [employees, setEmployees] = useState([]);
+
+  const addEmployee = (newEmployee) => {
+    setEmployees([...employees, newEmployee]);
+  };
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [job_title, setJobTitle] = useState("");
+  const [department, setDepartment] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const newEmployee = {
+        id: employees.length + 1,
+        firstName,
+        lastName,
+        email,
+        gender,
+        job_title,
+        department,
+      };
+      await axios.post("http://localhost:8080/employee/add", newEmployee);
+      addEmployee(newEmployee);
+      navigate("/");
+    } catch (error) {
+      console.error("Error adding employee:", error);
+    }
+  };
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: "grey", color: "white" }}>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Employee Management App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Card
+        sx={{
           marginTop: 4,
-          marginLeft:"28%",
-          borderColor:"black", 
-          position:"static",
-            backgroundColor: "White", 
-            color: "grey", 
-            maxWidth:620,
-            
-        }}>
-          <Box sx={{ 
-        flexGrow: 1, 
-        marginTop: 0}}>
-       <AppBar
-         position="static"
-         sx={{ backgroundColor: "lightgrey", color: "white" }}
-         >
-         <Toolbar>
-           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-             Add Employee 
-           </Typography>
-           <Button sx={{
-            color:'white',
-            background: "grey",
-            cursor: "pointer",
-                              "&:hover": {
-                                color: "",
-                                background: "#e6e2f0",
-                              },
-           }}
-           onClick={() => navigate(-1)}> Back
-               </Button>
-         </Toolbar>
-       </AppBar>
-     </Box>
-
-  <Box >
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        '& > :not(style)': { m: 1 },
-      }}
-    >
-      <TextField
-          id="standard-multiline-flexible"
-          label="First Name"
-          multiline
-          maxRows={8}
-          variant="standard"
-        />
-      
-       <TextField
-          id="standard-multiline-flexible"
-          label="Last Name"
-          multiline
-          maxRows={8}
-          variant="standard"
-        />
-     </Box>
-     <Box
-     sx={{
-      display: 'flex',
-      alignItems: 'center',
-      '& > :not(style)': { m: 1 },
-    }}
-     >
-      <TextField
-          id="standard-multiline-flexible"
-          label="Email"
-          //multiline
-          maxRows={1}
-          variant="standard"
-        />
-     </Box>
-
-     <Box
-     sx={{
-      display: 'flex',
-      alignItems: 'center',
-      '& > :not(style)': { m: 1 },
-    }}
-     >
-      <TextField
-          id="standard-multiline-flexible"
-          label="Gender"
-          //multiline
-          maxRows={1}
-          variant="standard"
-        />
-     </Box>
-     <Box
-     sx={{
-      display: 'flex',
-      alignItems: 'center',
-      '& > :not(style)': { m: 1 },
-    }}
-     >
-      <TextField
-          id="standard-multiline-flexible"
-          label="Job Title"
-          multiline
-          maxRows={8}
-          variant="standard"
-        />
-        <TextField
-          id="standard-multiline-flexible"
-          label="Department"
-          multiline
-          maxRows={8}
-          variant="standard"
-        />
-     </Box>
-     <Box
-     sx={{
-      '& > :not(style)': { m: 1 },
-      position: 'relative',
-          bottom: 0,
-          right: 0,
-          display: 'flex',
-          justifyContent: 'flex-end',
-          padding: '8px',
-    }}
-     >
-    <Button 
-      sx={{
-        color:'white',
-            background: "grey",
-            cursor: "pointer",
-                              "&:hover": {
-                                color: "",
-                                background: "#e6e2f0",
-                              }
-                            }}type="submit">
-          Add Employee
-      </Button>
-    <Button sx={{
-      color:'white',
-      background: "grey",
-      cursor: "pointer",
-                        "&:hover": {
-                          color: "",
-                          background: "#e6e2f0",
-                        },
-    }} 
-    type="button" onClick={handleCancel}>
-    Cancel
-    </Button>
-
-    </Box >
-    
-  </Box>
-      
+          marginLeft: "28%",
+          borderColor: "black",
+          position: "static",
+          backgroundColor: "White",
+          color: "grey",
+          maxWidth: 620,
+        }}
+      >
+        <Box sx={{ flexGrow: 1, marginTop: 0 }}>
+          <AppBar position="static" sx={{ backgroundColor: "lightgrey", color: "white" }}>
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Add Employee
+              </Typography>
+              <Button
+                sx={{
+                  color: "white",
+                  background: "grey",
+                  cursor: "pointer",
+                  "&:hover": { color: "", background: "#e6e2f0" },
+                }}
+                onClick={() => navigate(-1)}
+              >
+                Back
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <form onSubmit={onSubmit}>
+          <Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                "& > :not(style)": { m: 1 },
+              }}
+            >
+              <TextField
+                id="first-name"
+                label="First Name"
+                variant="standard"
+                name="first_name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+              <TextField
+                id="last-name"
+                label="Last Name"
+                variant="standard"
+                name="last_name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </Box>
+            {/* Other input fields */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                "& > :not(style)": { m: 1 },
+              }}
+            >
+              <TextField
+                id="email"
+                label="Email"
+                variant="standard"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                "& > :not(style)": { m: 1 },
+              }}
+            >
+              <TextField
+                id="gender"
+                label="Gender"
+                variant="standard"
+                name="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                required
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                "& > :not(style)": { m: 1 },
+              }}
+            >
+              <TextField
+                id="job-title"
+                label="Job Title"
+                variant="standard"
+                name="job_title"
+                value={job_title}
+                onChange={(e) => setJobTitle(e.target.value)}
+                required
+              />
+              <TextField
+                id="department"
+                label="Department"
+                variant="standard"
+                name="department"
+                value={department}
+                onChange={(e) => setDepartment(e.target.value)}
+                required
+              />
+           
+          </Box>
+          </Box>
+          <Box
+            sx={{
+              "& > :not(style)": { m: 1 },
+              position: "relative",
+              bottom: 0,
+              right: 0,
+              display: "flex",
+              justifyContent: "flex-end",
+              padding: "8px",
+            }}
+          >
+            <Button
+              type="submit"
+              sx={{
+                color: "white",
+                background: "grey",
+                cursor: "pointer",
+                "&:hover": { color: " blue", background: "#e6e2f0" },
+              }}
+            >
+              Submit
+            </Button>
+            <Button
+              sx={{
+                color: "white",
+                background: "grey",
+                cursor: "pointer",
+                "&:hover": { color: "red", background: "#e6e2f0" },
+              }}
+              type="button"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </Box>
+        </form>
       </Card>
-      </div>
-    )
+    </div>
+  );
 }
-// const[name, setName]= useState( ' ');
-    // const[last_name, setLastName]= useState( ' ');
-    // const[email, setEmail]= useState( ' ');
-    // const[gender, setGender]= useState( ' ');
-    // const[job_title, setJobtitle]= useState( ' ');
-    // const[department, setDepartment]= useState( ' ');
-   
-
-    // //import React from 'react';
-// import React, { useState,useEffect } from "react";
-// //import Modal from 'react-bootstrap/Modal';
-// import { useNavigate } from "react-router-dom";
-// import { Box, Button } from "@mui/material";
-// import Card from "@mui/material/Card";
-// import axios from "axios";
-// import AppBar from "@mui/material/AppBar";
-// import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
-// import TextField from "@mui/material/TextField";
-// import { useParams } from 'react-router-dom';
-// export default function View({match}) {
-//   useEffect(() => {
-//     // Disable scrolling when the component mounts
-//     document.body.style.overflow = "hidden";
-//     // Re-enable scrolling when the component unmounts
-//     return () => {
-//       document.body.style.overflow = "auto";
-//     };
-//   }, []);
-
-//   const navigate = useNavigate();
-
-//   const handleCancel = () => {
-//     // // Navigate back to the home page
-//     navigate("/");
-//   };
-
-//   const [data, setData] = useState(
-    
-//     {
-
-//       firstName: '',
-//     lastName: '',
-//     email: '',
-//     gender: '',
-//     jobTitle: '',
-//     department: ''
-//     });
-//   const { id } = useParams();
-   
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const res = await axios.get(`http://localhost:8080/employee/${id}`); // Replace with your backend endpoint
-//         const fetchedData = res.data; // Extract the fetched data from the response
-//         setData(fetchedData); // Set the fetched data to the state
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
-//     fetchData();
-//   }, [id]);
-
-//   return (
-//     <div>
-//       <Box
-//         sx={{
-//           flexGrow: 1,
-//         }}
-//       >
-//         <AppBar
-//           position="static"
-//           sx={{ backgroundColor: "grey", color: "white" }}
-//         >
-//           <Toolbar>
-//             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-//               Employee Managmment App
-//             </Typography>
-//           </Toolbar>
-//         </AppBar>
-//       </Box>
-//       <Card
-//         sx={{
-//           marginTop: 4,
-//           marginLeft: "28%",
-//           borderColor: "black",
-//           position: "static",
-//           backgroundColor: "White",
-//           color: "grey",
-//           maxWidth: 620,
-//         }}
-//       >
-//         <Box
-//           sx={{
-//             flexGrow: 1,
-//             marginTop: 0,
-//           }}
-//         >
-//           <AppBar
-//             position="static"
-//             sx={{ backgroundColor: "lightgrey", color: "white" }}
-//           >
-//             <Toolbar>
-//               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-//                 View Employee
-//               </Typography>
-//               <Button
-//                 sx={{
-//                   color: "white",
-//                   background: "grey",
-//                   cursor: "pointer",
-//                   "&:hover": {
-//                     color: "",
-//                     background: "#e6e2f0",
-//                   },
-//                 }}
-//                 onClick={() => navigate(-1)}
-//               >
-//                 {" "}
-//                 Back
-//               </Button>
-//             </Toolbar>
-//           </AppBar>
-//         </Box>
-
-//         <Box>
-//           <Box
-//             sx={{
-//               display: "flex",
-//               alignItems: "center",
-//               "& > :not(style)": { m: 1 },
-//             }}
-//           >
-//             <TextField
-//               id="standard-multiline-flexible"
-//               label="First Name"
-//               value={data.first_name}
-              
-//               variant="standard"
-//             />
-//             <TextField
-//               id="standard-multiline-flexible"
-//               label="Last Name"
-//               value={data.last_name}
-             
-//               variant="standard"
-//             />
-//           </Box>
-//           <Box
-//             sx={{
-//               display: "flex",
-//               alignItems: "center",
-//               "& > :not(style)": { m: 1 },
-//             }}
-//           >
-//             <TextField
-//               id="standard-multiline-flexible"
-//               label="Email"
-//               value={data.email}
-             
-//               variant="standard"
-//             />
-//           </Box>
-
-//           <Box
-//             sx={{
-//               display: "flex",
-//               alignItems: "center",
-//               "& > :not(style)": { m: 1 },
-//             }}
-//           >
-//             <TextField
-//               id="standard-multiline-flexible"
-//               label="Gender"
-//               value={data.gender}
-             
-//               variant="standard"
-//             />
-//           </Box>
-//           <Box
-//             sx={{
-//               display: "flex",
-//               alignItems: "center",
-//               "& > :not(style)": { m: 1 },
-//             }}
-//           >
-//             <TextField
-//               id="standard-multiline-flexible"
-//               label="Job Title"
-              
-//               value={data.job_title}
-            
-//               variant="standard"
-//             />
-//             <TextField
-//               id="standard-multiline-flexible"
-//               label="Department"
-//               value={data.department}
-            
-//               variant="standard"
-//               //onChange={(e) => setData(e.target.value)} // If you want to allow editing
-//             />
-//           </Box>
-
-
-//           <div>
-//       <h1>Employee Details</h1>
-//       <p>First Name: {data.first_name}</p>
-//       <p>Last Name: {data.last_name}</p>
-//       <p>Email: {data.email}</p>
-//       {/* Display other employee details as needed */}
-//     </div>
-//           <Box
-//             sx={{
-//               "& > :not(style)": { m: 1 },
-//               position: "relative",
-//               bottom: 0,
-//               right: 0,
-//               display: "flex",
-//               justifyContent: "flex-end",
-//               padding: "8px",
-//             }}
-//           >
-//             <Button
-//               sx={{
-//                 color: "white",
-//                 background: "grey",
-//                 cursor: "pointer",
-//                 "&:hover": {
-//                   color: "",
-//                   background: "#e6e2f0",
-//                 },
-//               }}
-//               type="button"
-//               onClick={handleCancel}
-//             >
-//               Cancel
-//             </Button>
-//      </Box>
-//         </Box>
-//       </Card>
-//     </div>
-//   );
-// }
